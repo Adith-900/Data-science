aa398e0ee1005c9e608b019d030379dbbd00f6c1
Adith-900 <adithmanojan900@gmail.com>
2025-08-21 16:54:06 +0530

    third commit

---
+++ ABC.py
@@ -0,0 +1,13 @@
+fruits=["Apple","Banana","Cherry","Pineapple","Jackfruit","Orange"]
+
+print(fruits[0])               #---first variable---
+print(fruits[2:5])             #---slice---
+print(len(fruits))             #---length---
+print(type(fruits))            #---data type---
+fruits.append("Lemon")         #---to add---
+fruits.append("Rambootan")
+fruits.pop()                  #---to remove---
+# fruits.remove("Apple")
+# print(fruits)
+print(fruits)                  #---to print all---
+
